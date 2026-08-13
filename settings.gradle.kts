8967e0c250165539dc726d5619f0a9ba444bb37e
Luci <63568477+LoopyLuci@users.noreply.github.com>
2026-08-12 22:37:27 -0400

    fix: use valid KSP version 2.0.1 instead of 2.2.0-2.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ pluginManagement {
         useVersion("2.55")
       }
       if (requested.id.id == "com.google.devtools.ksp") {
-        useVersion("2.2.0-2.0.1")
+        useVersion("2.0.1")
       }
     }
   }
